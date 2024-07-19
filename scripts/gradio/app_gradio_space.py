bf2aceb6d8cf0a5216bf568fc909a4aa9012e145
Murtuza Boriwala <murtuza.bori24@gmail.com>
2024-07-20 02:13:29 +0400

    Removed length constraint

--- scripts/gradio/app_gradio_space.py
+++ scripts/gradio/app_gradio_space.py
@@ -159,9 +159,9 @@ def limit_shape(image, input_w, input_h, img_edge_ratio, max_image_edge=max_imag
 def limit_length(length):
     """limite generation video frames numer to avoid gpu memory overflow"""
 
-    if length > 24 * 6:
-        gr.Warning("Length need to smaller than 144, dute to gpu memory limit")
-        length = 24 * 6
+    # if length > 24 * 6:
+    #     gr.Warning("Length need to smaller than 144, dute to gpu memory limit")
+    #     length = 24 * 6
     return length
 
 
