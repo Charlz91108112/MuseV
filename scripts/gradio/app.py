3e88d5abcc62a060c2d28af8197a5b75bb80d787
Murtuza Boriwala <murtuza.bori24@gmail.com>
2024-07-20 02:25:55 +0400

    removed video length constraint

--- scripts/gradio/app.py
+++ scripts/gradio/app.py
@@ -124,9 +124,9 @@ def limit_shape(image, input_w, input_h, img_edge_ratio, max_image_edge=max_imag
 def limit_length(length):
     """limite generation video frames numer to avoid gpu memory overflow"""
 
-    if length > 24 * 6:
-        gr.Warning("Length need to smaller than 144, dute to gpu memory limit")
-        length = 24 * 6
+    # if length > 24 * 6:
+    #     gr.Warning("Length need to smaller than 144, dute to gpu memory limit")
+    #     length = 24 * 6
     return length
 
 
@@ -184,26 +184,7 @@ css = """#input_img {max-width: 1024px !important} #output_vid {max-width: 1024p
 
 with gr.Blocks(css=css) as demo:
     gr.Markdown(
-        "<div align='center'> <h1> MuseV: Infinite-length and High Fidelity Virtual Human Video Generation with Visual Conditioned Parallel Denoising</span> </h1> \
-                    <h2 style='font-weight: 450; font-size: 1rem; margin: 0rem'>\
-                    </br>\
-                    Zhiqiang Xia <sup>*</sup>,\
-                    Zhaokang Chen<sup>*</sup>,\
-                    Bin Wu<sup>†</sup>,\
-                    Chao Li,\
-                    Kwok-Wai Hung,\
-                    Chao Zhan,\
-                    Yingjie He,\
-                    Wenjiang Zhou\
-                    (<sup>*</sup>Equal Contribution,  <sup>†</sup>Corresponding Author, benbinwu@tencent.com)\
-                    </br>\
-                    Lyra Lab, Tencent Music Entertainment\
-                </h2> \
-                <a style='font-size:18px;color: #000000' href='https://github.com/TMElyralab/MuseV'>[Github Repo]</a>\
-                <a style='font-size:18px;color: #000000'>, which is important to Open-Source projects. Thanks!</a>\
-                <a style='font-size:18px;color: #000000' href=''> [ArXiv(Coming Soon)] </a>\
-                <a style='font-size:18px;color: #000000' href=''> [Project Page(Coming Soon)] </a> \
-                <a style='font-size:18px;color: #000000'>If MuseV is useful, please help star the repo~ </a> </div>"
+        "<div align='center'> <h1> MuseV: Genomi Avatar Video Generation</span> </h1> </div>"
     )
     with gr.Tab("Text to Video"):
         with gr.Row():
